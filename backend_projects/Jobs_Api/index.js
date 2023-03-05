0c502f36bbe006fb6b4f4af42e2621ec4e83524c
Sophiya Singh <sophiya.singh2002@gmail.com>
2023-03-06 00:29:50 +0530

    final backend

--- backend_projects/Jobs_Api/index.js
+++ backend_projects/Jobs_Api/index.js
@@ -1,39 +1,46 @@
 require('dotenv').config();
 require('express-async-errors');
-const express = require('express')
-const app = express();
 
 const helmet = require('helmet')
 const cors = require('cors')
 const xss = require('xss-clean')
-const rateLimiter = require('express-rate-limiter')
+const rateLimiter = require('express-rate-limit')
+
+const express = require('express')
+const app = express();
 
 const connectDb = require('./db/connect');
-
+const authenticateUser = require('./middleware/authentication')
 const authRouter= require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
-const authenticateUser = require('./middleware/authentication')
-
 const notFoundMiddleware = require('./middleware/notFound');
 const errorHandlerMiddleware = require('./middleware/errorHandler');
+const path = require('path');
 
 
-app.get('/', (req, res) =>{
-    res.send('jobs api')
-});
+app.set('trustProxy', 1)
+app.use(
+    rateLimiter({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    })
+  );
+app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
+app.get('/', (req,res) => {
+    res.send('<h1> Jobs Api</h1>');
+})
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-app.set('trustProxy', 1)
-app.use(rateLimiter)
-app.use(express.json());
-app.use(helmet())
-app.use(cors())
-app.use(xss())
 
 
 const port = process.env.PORT || 3000;
@@ -49,3 +56,5 @@ const connect = async () =>{
     }
 }
 connect();
+
+// module.exports.handler =serverless(app);
